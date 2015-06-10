9bf669feb4b79b4c553fab79ac2dc895135824bb
Richard Doe <richard.doe@rwdit.net>
2015-06-10 11:41:27 +0100

    Omit empty params from MLT query.

--- lib/europeana/blacklight/document/more_like_this.rb
+++ lib/europeana/blacklight/document/more_like_this.rb
@@ -7,7 +7,6 @@ module Europeana
         ##
         # Queries the API for items similar to a given document
         #
-        # @param [Europeana::Blacklight::Document] doc
         # @return [Array<Europeana::Blacklight::Document>]
         def more_like_this
           mlt_params = { query: more_like_this_query, rows: 4, profile: 'rich' }
@@ -16,18 +15,13 @@ module Europeana
           repository.search(mlt_params).documents
         end
 
-        # @param [Europeana::Blacklight::Document] doc
         # @return [String]
         def more_like_this_query
-          field_queries = more_like_this_logic.map do |component|
-            query = "#{component[:param]}: ("
-            [component[:fields]].flatten.each do |field|
-              query << fetch(field, []).compact.map { |v| '"' + v + '"' }.join(' OR ')
-            end
-            query << ")^#{component[:boost]}"
-          end
-          '(' + field_queries.join(' OR ') + ') NOT europeana_id:"' + self.id + '"'
+          field_queries = more_like_this_field_queries.join(' OR ')
+          "(#{field_queries}) NOT europeana_id:\"#{self.id}\""
         end
+
+        protected
 
         def more_like_this_logic
           [
@@ -36,6 +30,25 @@ module Europeana
             { param: 'DATA_PROVIDER', fields: 'aggregations.edmDataProvider', boost: 0.2 },
             { param: 'what', fields: ['proxies.dcType', 'proxies.dcSubject', 'concepts.about'], boost: 0.8 },
           ]
+        end
+
+        def more_like_this_field_terms(*fields)
+          fields.flatten.map do |field|
+            fetch(field, []).compact
+          end.flatten
+        end
+
+        def more_like_this_field_queries
+          more_like_this_logic.map do |component|
+            field_terms = more_like_this_field_terms(component[:fields])
+            more_like_this_param_query(component[:param], field_terms, component[:boost])
+          end.compact
+        end
+
+        def more_like_this_param_query(param, terms, boost)
+          return nil unless terms.present?
+          or_terms = terms.map { |v| '"' + v + '"' }.join(' OR ')
+          "#{param}: (#{or_terms})^#{boost}"
         end
       end
     end
