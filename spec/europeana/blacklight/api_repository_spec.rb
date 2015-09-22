1ce669c58e5e2735e77954dcd47a6b295d1b1001
Richard Doe <richard.doe@rwdit.net>
2015-09-22 10:38:40 +0100

    Style.

--- spec/europeana/blacklight/api_repository_spec.rb
+++ spec/europeana/blacklight/api_repository_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe Europeana::Blacklight::ApiRepository do
     it 'should send a record query to the API' do
       subject.find(record_id)
       expect(a_request(:get, "www.europeana.eu/api/v2/record#{record_id}.json").
-        with(:query => hash_including({'wskey' => api_key}))).to have_been_made
+        with(query: hash_including({ 'wskey' => api_key }))).to have_been_made
     end
 
     it 'should pass on API query params' do
       subject.find(record_id, callback: 'showRecord')
       expect(a_request(:get, "www.europeana.eu/api/v2/record#{record_id}.json").
-        with(:query => hash_including({'wskey' => api_key, 'callback' => 'showRecord'}))).to have_been_made
+        with(query: hash_including({ 'wskey' => api_key, 'callback' => 'showRecord' }))).to have_been_made
     end
 
     it 'should return configured response model' do
@@ -45,7 +45,7 @@ RSpec.describe Europeana::Blacklight::ApiRepository do
     it 'should send a search query to the API' do
       subject.search('query' => query)
       expect(a_request(:get, 'www.europeana.eu/api/v2/search.json').
-        with(:query => hash_including({'wskey' => api_key, 'query' => query}))).to have_been_made
+        with(query: hash_including({ 'wskey' => api_key, 'query' => query }))).to have_been_made
     end
 
     it 'should return configured response model' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,10 @@ RSpec.configure do |config|
   config.before(:each) do
     stub_request(:get, %r{http://www.europeana.eu/api/v2/record/[^/]+/[^/.]+\.json}).
       with(query: hash_including(:wskey)).
-      to_return(body: '{"success":true}', headers: {'content-type' => 'application/json'})
+      to_return(body: '{"success":true}', headers: { 'content-type' => 'application/json' })
 
     stub_request(:get, 'http://www.europeana.eu/api/v2/search.json').
       with(query: hash_including(:wskey, :query)).
-      to_return(body: '{"success":true}', headers: {'content-type' => 'application/json'})
+      to_return(body: '{"success":true}', headers: { 'content-type' => 'application/json' })
   end
 end
