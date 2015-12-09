fb216cda02d762e8d71b76844ed719174ff75c4e
Richard Doe <richard.doe@rwdit.net>
2015-12-09 16:32:03 +0000

    Allow passing of Blacklight document as mlt param

--- lib/europeana/blacklight/search_builder/more_like_this.rb
+++ lib/europeana/blacklight/search_builder/more_like_this.rb
@@ -13,8 +13,12 @@ module Europeana
         def add_mlt_to_api(api_parameters)
           return unless blacklight_params[:mlt]
           repository = blacklight_config.repository_class.new(blacklight_config)
-          doc_response = repository.find(blacklight_params[:mlt])
-          query = doc_response.documents.first.more_like_this_query(blacklight_params[:mltf])
+          if blacklight_params[:mlt].respond_to?(:more_like_this_query)
+            query = blacklight_params[:mlt].more_like_this_query
+          else
+            doc_response = repository.find(blacklight_params[:mlt])
+            query = doc_response.documents.first.more_like_this_query(blacklight_params[:mltf])
+          end
           append_to_query_param(api_parameters, query)
         end
       end
