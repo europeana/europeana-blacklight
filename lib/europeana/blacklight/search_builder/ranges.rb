d83f63ea45ff2da60d03ebb3c908a5a22316f9dd
Richard Doe <richard.doe@rwdit.net>
2016-09-20 12:54:31 +0100

    Handle empty range values
    By passing '*' to the API for Lucene open-ended range queries.

--- lib/europeana/blacklight/search_builder/ranges.rb
+++ lib/europeana/blacklight/search_builder/ranges.rb
@@ -13,8 +13,10 @@ module Europeana
         def add_range_qf_to_api(api_parameters)
           return unless blacklight_params.key?(:range) && blacklight_params[:range].is_a?(Hash)
           blacklight_params[:range].each_pair do |range_field, range_values|
+            range_begin = range_values[:begin].blank? ? '*' : range_values[:begin]
+            range_end = range_values[:end].blank? ? '*' : range_values[:end]
             api_parameters[:qf] ||= []
-            api_parameters[:qf] << "#{range_field}:[#{range_values[:begin]} TO #{range_values[:end]}]"
+            api_parameters[:qf] << "#{range_field}:[#{range_begin} TO #{range_end}]"
           end
         end
       end
